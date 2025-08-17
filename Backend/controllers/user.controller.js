9a7d84cf85736a7774a53334d3220bee8ae9792a
Sanket Mane <sanketmane2255@gmail.com>
2025-08-17 22:27:11 +0530

    User registration completed

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,1 +1,32 @@
 const userModel = require('../models/user.model'); 
+const userService = require('../services/user.service');
+const {validationResult} = require('express-validator');
+
+
+
+module.exports.registerUser = async (req , res , next) => {
+
+     console.log("Registering user", req.body);
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()});
+    }
+
+    console.log("Registering user", req.body);
+
+    const {fullname, email, password} = req.body;
+
+    const hashedPassword = await userModel.hashPassword(password);
+
+    const user = await userService.createUser({
+        firstname:fullname.firstname,
+        lastname:fullname.lastname,
+        email,
+        password: hashedPassword
+    });
+     
+    const token = user.generateAuthToken();
+
+    res.status(201).json({ user, token });
+}
