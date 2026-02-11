a08555efa7e1b82db33a3b21f344b71d3f4d4e9f
Sanket Mane <sanketmane2255@gmail.com>
2026-02-11 08:08:28 +0530

    user auth done, added profile,logout,middleware

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const authMiddleware = require('../middlewares/auth.middleware');
 const {body} = require('express-validator');
 const userController = require('../controllers/user.controller');
 
@@ -15,5 +15,9 @@ router.post('/login', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
 ], userController.loginUser);
+
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile);
+
+router.get('/logout', authMiddleware.authUser, userController.logoutUser);
 
 module.exports = router;
